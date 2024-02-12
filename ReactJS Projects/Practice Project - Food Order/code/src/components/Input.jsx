3a197d914d866058b9a554d147d5b2826d9c5928
Mikula Mikula <stefankm99@gmail.com>
2024-02-12 20:21:55 +0100

    commit projects

--- ReactJS Projects/Practice Project - Food Order/code/src/components/Input.jsx
+++ ReactJS Projects/Practice Project - Food Order/code/src/components/Input.jsx
@@ -2,7 +2,8 @@ export default function Input({ label, id, error , ...props }) {
     return (
         <>
             <label htmlFor={id}>{label}</label>
-            <input id={id} {...props} />            
+            <input id={id} {...props} />  
+            {error && <p className ="error">{error}</p>}           
         </>
     );
 }
